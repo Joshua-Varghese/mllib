a9e4812a9560a10a81e5402f6eba1768d29044aa
Joshua Varghese <joshuadude2715@gmail.com>
2026-04-07 21:08:37 +0530

    make the variables and objects be named after the book for easy reference

--- find-s.py
+++ find-s.py
@@ -1,23 +1,19 @@
-def find_s(c):
+def find_s(D):
     #assuming that the last column would be the target concept
     flag = True
-    hypo_target = [0]*(len(c[0])-1)
+    hypothesis = [0]*(len(D[0])-1)
       
-    for hypo in c:
-        if hypo[-1] in ["yes","Yes","YES"]:
-            bucket = hypo[:-1].copy()
+    for X in D:
+        if X[-1] in ["yes","Yes","YES"]:
+            bucket = X[:-1].copy()
             if flag == True:
-                hypo_target = bucket
+                hypothesis = bucket
                 flag = False
             else:
                 for elem in range(len(bucket)):
-                    if hypo_target[elem] == bucket[elem]:
+                    if hypothesis[elem] == bucket[elem]:
                         pass
                     else:
-                        hypo_target[elem] = "?"
-    print(hypo_target)
-
-        
-
-
+                        hypothesis[elem] = "?"
+    print(hypothesis)
 
